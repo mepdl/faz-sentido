663507181ae47dde208a2df79e502d1032e38745
Marcos Eduardo <marcos.epdl0712@gmail.com>
2026-02-23 15:00:30 -0400

    feat: adicionando vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/react";
 
 import Home from "@/pages/Home";
 import PostDetail from "@/pages/PostDetail";
@@ -54,6 +55,7 @@ function App() {
       <TooltipProvider>
         <Toaster />
         <Router />
+        <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
   );
